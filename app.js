074bf5ad30058ed9fca73487608b5becc7295fb9
feud72 <feud72@gmail.com>
2019-10-10 16:41:08 +0900

    #2.8

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("I'm home.");
 
@@ -23,5 +19,6 @@ app.use(morgan("dev"));
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
+app.use("/user", userRouter);
 
-app.listen(PORT, handleListening);
+export default app;
