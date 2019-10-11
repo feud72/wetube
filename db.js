a3fbe9b2d4b5005437cbed247f9c8a95ed79a40e
feud72 <feud72@gmail.com>
2019-10-11 23:28:45 +0900

    #2.22

--- db.js
+++ db.js
@@ -4,19 +4,20 @@ export const videos = [
     title: "Video awesome",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    id: 121212,
+    name: "Nicolas",
+    email: "nico@las.com"
   },
+
   {
     id: 1212121,
     title: "Video super",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "Nicolas",
@@ -28,7 +29,8 @@ export const videos = [
     title: "Video nice",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "Nicolas",
@@ -40,7 +42,8 @@ export const videos = [
     title: "Video perfect",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "Nicolas",
