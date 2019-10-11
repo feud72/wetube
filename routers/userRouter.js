a008555ca8ecd1fcbe2ecf71dd19af1ce81531ca
feud72 <feud72@gmail.com>
2019-10-11 11:34:46 +0900

    #2.15

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,13 +1,15 @@
 import express from "express";
 import routes from "../routes";
+import {
+  userDetail,
+  editProfile,
+  changePassword
+} from "../controller/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send("Users"));
-userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
-userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
-userRouter.get(routes.changePassword, (req, res) =>
-  res.send("Change Password")
-);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
