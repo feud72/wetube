19956b66785e8b7c37445a7bf16a181926502283
feud72 <feud72@gmail.com>
2019-10-15 16:10:32 +0900

    #2.23

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,22 @@
-export const join = (req, res) => res.render("join");
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join");
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.render("logout");
 export const userDetail = (req, res) => res.render("userDetail");
