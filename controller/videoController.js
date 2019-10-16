b44672018a8cadbfbbcee52f4ae6aed0e0ae477b
feud72 <feud72@gmail.com>
2019-10-16 17:05:56 +0900

    #3.6

--- controller/videoController.js
+++ controller/videoController.js
@@ -20,10 +20,18 @@ export const search = (req, res) => {
 
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
-export const postUpload = (req, res) => {
-  const { file, title, description } = req.body;
-  // To Do: upload and save video
-  res.redirect(routes.videoDetail(324393));
+
+export const postUpload = async (req, res) => {
+  const {
+    body: { title, description },
+    file: { path }
+  } = req;
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  });
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -8,11 +8,12 @@ import {
   getUpload,
   postUpload
 } from "../controller/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
