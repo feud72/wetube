19d8450f3bde76d5f44fb7323def6829a3ba224d
feud72 <feud72@gmail.com>
2019-10-17 15:07:30 +0900

    #3.12

--- controller/videoController.js
+++ controller/videoController.js
@@ -11,11 +11,18 @@ export const home = async (req, res) => {
   }
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  const videos = {};
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" }
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
