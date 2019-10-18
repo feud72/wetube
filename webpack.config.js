164ad032f94abea6c50fd00f2fe3cbe484512fe1
feud72 <feud72@gmail.com>
2019-10-18 10:22:34 +0900

    #4.3

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,18 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
       {
         test: /\.(scss)$/,
         use: ExtractCSS.extract([
@@ -21,7 +29,7 @@ const config = {
           {
             loader: "postcss-loader",
             options: {
-              plugin() {
+              plugins() {
                 return [autoprefixer({ browsers: "cover 99.5%" })];
               }
             }
@@ -35,7 +43,7 @@ const config = {
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]"
+    filename: "[name].js"
   },
   plugins: [new ExtractCSS("styles.css")]
 };
